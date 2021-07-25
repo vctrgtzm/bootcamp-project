7e44894868ecb70f5039005fae975e8c3768af30
Victor Gutierrez <vctrgtzm@gmail.com>
2021-07-25 15:54:58 -0500

    Make Header component tests pass

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,20 @@
 
+
 function Header() {
+    const logo = require('../../logo.png');
     return (
-        <header data-testid="header"></header>
+        <header data-testid="header">
+            <img data-testid="hd-logo" alt="the icon of the app" src={logo}></img>
+            <nav data-testid="hd-nav-var">
+                <ul>
+                    <li>Home</li>
+                    <li>Favorites</li>
+                </ul>
+            </nav>
+            <input data-testid="hd-search-input" type="text" placeholder="Search..." />
+            <input data-testid="hd-theme-toggle" type="checkbox" />
+            <div data-testid="hd-avatar"></div>
+        </header>
     );
 
 }
