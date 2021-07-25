94b4b4002e3501daf8d9b8101342d7af6f1c50f2
Victor Gutierrez <vctrgtzm@gmail.com>
2021-07-25 15:54:28 -0500

    Remove unnecessary import and fixed syntax error

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,4 +1,4 @@
-import { queryByTestId, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
 describe('Header', () => {
@@ -12,26 +12,26 @@ describe('Header', () => {
     test('should render navigation bar', () => {
         render(<Header />);
 
-        const navBar = screen.queryByTestId("hd-nav-var")
+        const navBar = screen.queryByTestId("hd-nav-var");
         expect(navBar).toBeInTheDocument();
     });
 
-    test('should render search input'){
-        render(<Header/>);
+    test('should render search input', () => {
+        render(<Header />);
 
         const searchInput = screen.queryByTestId("hd-search-input");
         expect(searchInput).toBeInTheDocument();
-    }
+    });
 
     test('should render theme toggle', () => {
-        render(<Header/>);
+        render(<Header />);
 
         const themeToggle = screen.queryByTestId("hd-theme-toggle");
         expect(themeToggle).toBeInTheDocument();
     });
 
     test('should render avatar', () => {
-        render(<Header/>);
+        render(<Header />);
 
         const avatar = screen.queryByTestId("hd-avatar");
         expect(avatar).toBeInTheDocument();
