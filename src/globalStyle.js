87f9796d6f37465675091382525ce28ef3696d17
Victor Gutierrez <vctrgtzm@gmail.com>
2021-07-25 20:48:51 -0500

    Import font and fix document identing

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,10 +1,16 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap');
+
     * {
         margin: 0;
         padding: 0;
-        box-sizing: border-box;
+        box-sizing: border-box;        
+    }
+
+    *:focus{
+        outline: none;
     }
 `;
 
