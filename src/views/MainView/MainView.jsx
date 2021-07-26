7f967d54df1d86d2d3f2abada88a4325020e9aee
Victor Gutierrez <vctrgtzm@gmail.com>
2021-07-26 11:51:50 -0500

    Make test pass

--- src/views/MainView/MainView.jsx
+++ src/views/MainView/MainView.jsx
@@ -1,6 +1,14 @@
 
 function MainView(){
-    return <section data-testid="main-view"></section>
+    const mockData = require('../../mocks/youtube-videos-mock.json');
+
+    return <section data-testid="main-view">
+        <div>
+            {mockData.items.map((item, idx) => {
+                return <div key={idx} data-testid="video-container">{item.snippet.title}</div>
+            })}
+        </div>
+    </section>
 }
 
 export default MainView;
