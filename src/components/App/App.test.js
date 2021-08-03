aaa42fdbed4def17acdf6237f09867c2e906b374
Victor Gutierrez <vctrgtzm@gmail.com>
2021-08-03 12:17:31 -0500

    Add todo test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,20 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
 describe('App', () => {
-    test('should render header', () => {
-        render(<App />);
 
-        const headerComponent = screen.queryByTestId("header");
-        expect(headerComponent).toBeInTheDocument();
-    });
-
-    test('should render the header', () => {
-        render(<App />);
-
-        const headerComponent = screen.queryByRole("banner");
-        expect(headerComponent).toBeInTheDocument();
-    });
+    test.todo('should render the header');
 
     test.todo('should render a view');
 });
