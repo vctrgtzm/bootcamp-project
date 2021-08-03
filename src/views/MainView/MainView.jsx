fd395027acb6f7fe314a90bfbd5bcf8d4edc5010
Victor Gutierrez <vctrgtzm@gmail.com>
2021-08-03 11:33:06 -0500

    Add loading indicator and error container in case something went wrong on the search request

--- src/views/MainView/MainView.jsx
+++ src/views/MainView/MainView.jsx
@@ -1,12 +1,27 @@
 import VideoGrid from '../../components/VideoGrid/VideoGrid';
+import { LoadingIndicator, ErrorContainer } from '../../components/App/App.styled';
 
-function MainView() {
-    const mockData = require('../../mocks/youtube-videos-mock.json');
+
+function MainView({ searchIsLoading, searchResult, searchError, setVideoId }) {
+
+    if (searchIsLoading) {
+        return <LoadingIndicator />
+    }
+
+    if (searchError) {
+        return (
+            <ErrorContainer>
+                <h1>Something went wrong when fetching data from youtube API :(</h1>
+                <p>{searchError}</p>
+            </ErrorContainer>
+        );
+    }
 
     return (
         <div role="main">
             <VideoGrid
-                items={mockData.items}
+                items={searchResult.items}
+                setVideoId={setVideoId}
             />
         </div>
     );
