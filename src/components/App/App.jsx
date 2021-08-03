a34affc0fba76eb6798dc17bd8750cf7dff504f9
Victor Gutierrez <vctrgtzm@gmail.com>
2021-08-03 10:02:28 -0500

    Use custom hooks to retrieve data from youtube api

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,47 @@
+
+import { useYoutubeSearch, useYoutubeVideo } from "../../customHooks/useYoutube";
 import MainView from "../../views/MainView";
+import VideoDetails from "../../views/VideDetails";
 import Header from "../Header";
 
 function App() {
+    const {
+        searchResult,
+        searchIsLoading,
+        searchError,
+        setSearchTerm
+    } = useYoutubeSearch('wizeline', 50); //default search term 'wizeline' with default max result 50
+
+    const {
+        videoData,
+        videoIsLoading,
+        videoError,
+        videoId,
+        setVideoId
+    } = useYoutubeVideo();
+
     return (
         <>
-            <Header />
-            <MainView />
+            <Header
+                setSearchTerm={setSearchTerm}
+                setVideoId={setVideoId}
+            />
+            {videoId == null ? (
+                <MainView
+                    searchIsLoading={searchIsLoading}
+                    searchResult={searchResult}
+                    searchError={searchError}
+                    setVideoId={setVideoId}
+                />
+            ) : (
+                <VideoDetails
+                    videoData={videoData}
+                    videoIsLoading={videoIsLoading}
+                    videoError={videoError}
+                    setVideoId={setVideoId}
+                />
+            )}
+
         </>
     );
 }
