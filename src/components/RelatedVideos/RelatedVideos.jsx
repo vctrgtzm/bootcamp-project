266f9c5b97505540f5ba63cc463ce95e9f6fefcc
Victor Gutierrez <49415800+vctrgtzm@users.noreply.github.com>
2021-08-09 14:13:00 -0500

    Added role to related videos

--- src/components/RelatedVideos/RelatedVideos.jsx
+++ src/components/RelatedVideos/RelatedVideos.jsx
@@ -6,7 +6,11 @@ const RelatedVideos = ({ relatedVideosResult, setVideoId }) => {
         <RelatedVideosContainer>
             {relatedVideosResult.items.filter(item => item.snippet !== undefined).map(item => { //filter results without snippet
                 return (
-                    <RelatedVideoContainer onClick={() => setVideoId(item.id.videoId)} key={item.id.videoId}>
+                    <RelatedVideoContainer
+                        role="listitem"
+                        onClick={() => setVideoId(item.id.videoId)}
+                        key={item.id.videoId}
+                    >
                         <ThumbnailContainer thumbnail={item.snippet.thumbnails?.high?.url || item.snippet.thumbnails?.medium?.url || item.snippet.thumbnails?.default?.url} />
                         <span>{item.snippet.title}</span>
                     </RelatedVideoContainer>
