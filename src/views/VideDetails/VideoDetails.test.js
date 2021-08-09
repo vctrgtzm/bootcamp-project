e7452ced48234116eb0c85a9f69dbaa77f7e5ad4
Victor Gutierrez <49415800+vctrgtzm@users.noreply.github.com>
2021-08-09 14:13:33 -0500

    Created tests for 'VideoDetails' view

--- src/views/VideDetails/VideoDetails.test.js
+++ src/views/VideDetails/VideoDetails.test.js
@@ -1,12 +1,132 @@
+import { render, screen } from "@testing-library/react";
+import * as useYoutubeHooks from "../../customHooks/useYoutube";
+import VideoDetails from "./VideoDetails";
+
+const mockVideoData = require('../../mocks/youtube-video-details-mock.json');
 
 describe('VideoDetails', () => {
 
-    test.todo('should render the video player');
+    describe('when data is being fetched', () => {
+        test('the loading indicator should be rendered', () => {
+            render(<VideoDetails
+                videoData={null}
+                videoIsLoading={true}
+                videoError={null}
+                setVideoId={jest.fn()}
+            />);
 
-    test.todo('should render the "back to videos" button');
+            const loadingIndicator = screen.queryByRole('progressbar');
+            expect(loadingIndicator).toBeInTheDocument();
+        });
+    });
 
-    test.todo('should render the title and description of the video');
+    describe('when data has been fetched', () => {
+        test('should render the video player', () => {
 
-    test.todo('should render the list of related videos contained in a result of a search request');
+            jest.spyOn(useYoutubeHooks, 'useYoutubeRelatedVideos').mockImplementation(() => ({
+                relatedVideosResult: {items: []},
+                relatedVideosIsLoading: false,
+                relatedVideosError: null,
+                setMaxRelatedVideosResults: () => {}
+            })); 
+
+            render(<VideoDetails
+                videoData={mockVideoData}
+                videoIsLoading={false}
+                videoError={null}
+                setVideoId={jest.fn()}
+            />)
+
+            const videoPlayer = document.querySelector(`iframe[src="//www.youtube.com/embed/${mockVideoData.items[0].id}"]`);
+            expect(videoPlayer).toBeInTheDocument();
+        });
+
+        test('should render the "back to videos" button', () => {
+
+            jest.spyOn(useYoutubeHooks, 'useYoutubeRelatedVideos').mockImplementation(() => ({
+                relatedVideosResult: {items: []},
+                relatedVideosIsLoading: false,
+                relatedVideosError: null,
+                setMaxRelatedVideosResults: () => {}
+            })); 
+
+            render(<VideoDetails
+                videoData={mockVideoData}
+                videoIsLoading={false}
+                videoError={null}
+                setVideoId={jest.fn()}
+            />)
+
+            const backToVideosButton = screen.queryByText('Back to videos');
+            expect(backToVideosButton).toBeInTheDocument();
+
+        });
+
+        test('should render the title and description of the video', () => {
+            jest.spyOn(useYoutubeHooks, 'useYoutubeRelatedVideos').mockImplementation(() => ({
+                relatedVideosResult: {items: []},
+                relatedVideosIsLoading: false,
+                relatedVideosError: null,
+                setMaxRelatedVideosResults: () => {}
+            })); 
+
+            render(<VideoDetails
+                videoData={mockVideoData}
+                videoIsLoading={false}
+                videoError={null}
+                setVideoId={jest.fn()}
+            />)
+
+            const title = screen.queryByText(mockVideoData.items[0].snippet.title);
+            expect(title).toBeInTheDocument();
+            const description = screen.queryByText(mockVideoData.items[0].snippet.description);
+            expect(description).toBeInTheDocument();
+        });
+
+        test('should render the list of related videos contained in a result of a search request', () =>{
+            const relatedVideosDataMock = require('../../mocks/youtube-related-videos-mock.json')
+
+            jest.spyOn(useYoutubeHooks, 'useYoutubeRelatedVideos').mockImplementation(() => ({
+                relatedVideosResult: relatedVideosDataMock,
+                relatedVideosIsLoading: false,
+                relatedVideosError: null,
+                setMaxRelatedVideosResults: () => {}
+            })); 
+
+            render(<VideoDetails
+                videoData={mockVideoData}
+                videoIsLoading={false}
+                videoError={null}
+                setVideoId={jest.fn()}
+            />)
+
+            const relatedVideosList = screen.queryAllByRole('listitem');
+            expect(relatedVideosList).toHaveLength(relatedVideosDataMock.items.length);
+        });
+    });
+
+    describe('when an error ocurred during fetching', () => {
+        test('the error message should be rendered', () => {
+            jest.spyOn(useYoutubeHooks, 'useYoutubeRelatedVideos').mockImplementation(() => ({
+                relatedVideosResult: null,
+                relatedVideosIsLoading: false,
+                relatedVideosError: 'Error fetching',
+                setMaxRelatedVideosResults: () => {}
+            }));    
+            render(<VideoDetails
+                videoData={null}
+                videoIsLoading={false}
+                videoError={'Error fetching'}
+                setVideoId={jest.fn()}
+            />);
+
+            const errorMessage = screen.queryByText('Something went wrong when fetching data from youtube API :(');
+            expect(errorMessage).toBeInTheDocument();
+        });
+    });
+
+
+
+
 
 });
