2199ffb08970162d53b4bd1774481960df4015b1
Victor Gutierrez <49415800+vctrgtzm@users.noreply.github.com>
2021-08-29 22:50:54 -0500

    Add favorite videos in user info

--- src/api/login.api.js
+++ src/api/login.api.js
@@ -2,6 +2,7 @@ const mockedUser = {
     id: '123',
     name: 'Wizeline',
     avatarUrl: 'https://media.glassdoor.com/sqll/868055/wizeline-squarelogo-1473976610815.png',
+    favoriteVideos: []
   };
   
   export default async function loginApi(username, password) {
